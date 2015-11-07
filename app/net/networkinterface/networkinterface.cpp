bc993cebd51ea5ea266eadcac27f92cb121b243e
gilgil <gilgil@gilgil.net>
2015-11-08 05:30:06 +0900

    ...

--- app/net/networkinterface/networkinterface.cpp
+++ app/net/networkinterface/networkinterface.cpp
@@ -7,21 +7,20 @@ int main()
   for (GNetworkInterfaces::iterator it = intfs.begin(); it != intfs.end(); it++) {
     GNetworkInterface& intf = *it;
 
-      // printf("Inteface #%d%s\n", i, intf.name == interfaces.at(0).name ? " (Best)" : ""); // gilgil temp 2015.11.08
+    // printf("Inteface #%d%s\n", i, intf.name == interfaces.at(0).name ? " (Best)" : ""); // gilgil temp 2015.11.08
+    if (intf.name_ != "")
+      printf("  name        = %s\n", qPrintable(intf.name_));
+    if (intf.description_ !=  "")
+      printf("  description = %s\n", qPrintable(intf.description_));
 
-      if (intf.description_ !=  "")
-        printf("  description = %s\n", qPrintable(intf.description_));
-      if (intf.name_ != "")
-        printf("  name        = %s\n", qPrintable(intf.name_));
-
-      if (!intf.mac_.isClean())
-        printf("  mac         = %s\n", qPrintable(intf.mac_));
-      if (intf.ip_ != 0)
-        printf("  ip          = %s\n", qPrintable(intf.ip_));
-      if (intf.subnet_ != 0)
-        printf("  subnet      = %s\n", qPrintable(intf.subnet_));
-      if (intf.gateway_ != 0)
-        printf("  gateway     = %s\n", qPrintable(intf.gateway_));
-      printf("\n");
+    if (!intf.mac_.isClean())
+      printf("  mac         = %s\n", qPrintable(intf.mac_));
+    if (intf.ip_ != 0)
+      printf("  ip          = %s\n", qPrintable(intf.ip_));
+    if (intf.subnet_ != 0)
+      printf("  subnet      = %s\n", qPrintable(intf.subnet_));
+    if (intf.gateway_ != 0)
+      printf("  gateway     = %s\n", qPrintable(intf.gateway_));
+    printf("\n");
   }
 }
