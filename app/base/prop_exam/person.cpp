eb9db783ef291a0035da0fa2d33a0edd261f6f37
gilgil <gilgil@gilgil.net>
2016-01-26 16:46:11 +0900

    Change propCreatePItem > propCreateItem

--- app/base/prop_exam/person.cpp
+++ app/base/prop_exam/person.cpp
@@ -3,13 +3,13 @@
 
 #ifdef QT_GUI_LIB
 
-GPropItem* Person::propCreatePItem(GPropItemParam param) {
+GPropItem* Person::propCreateItem(GPropItemParam param) {
   if (QString(param.mpro_.name()) == "picture") {
     GPropItemFilePath* res = new GPropItemFilePath(param);
     res->fd_->setNameFilter("Image files - *.png *.xpm *.jpg (*.png *.xpm *.jpg);;Any files - * (*)");
     return res;
   }
-  return GObj::propCreatePItem(param);
+  return GObj::propCreateItem(param);
 }
 
 #endif // QT_GUI_LIB
--- app/base/prop_exam/person.h
+++ app/base/prop_exam/person.h
@@ -24,6 +24,6 @@ public:
   QString picture_;
 
 #ifdef QT_GUI_LIB
-  GPropItem* propCreatePItem(GPropItemParam param) override;
+  GPropItem* propCreateItem(GPropItemParam param) override;
 #endif // QT_GUI_LIB
 };
