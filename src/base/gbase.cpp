32a0ad9166131051e56c883706fd816a66265ebd
gilgil <gilgil@gilgil.net>
2016-09-27 04:52:35 +0900

    Change G_TIMEOUT from 10 sec to 5 sec

--- src/base/gbase.cpp
+++ src/base/gbase.cpp
@@ -6,4 +6,4 @@ const char* G_VERSION = "G Library version 0.1 Debug   Build( " __DATE__ " " __T
 const char* G_VERSION = "G Library version 0.1 Release Build( " __DATE__ " " __TIME__ " )";
 #endif // _DEBUG
 
-unsigned long G_TIMEOUT = 10000; // 10 sec
+unsigned long G_TIMEOUT = 5000; // 5 sec
