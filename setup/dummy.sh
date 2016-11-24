d6295dbcbb6f492eab265593124f90bd384b0650
gilgil <gilgil@gilgil.net>
2016-11-25 00:47:02 +0900

    Update add and delete codes

--- setup/dummy.sh
+++ setup/dummy.sh
@@ -2,10 +2,10 @@ COMMAND=$1
 INTERFACE=$2
 
 function usage() {
-	echo "usage : dummy <install|uninstall|add|del> [<interface>]"
+	echo "usage : dummy <start|stop|add|del> [<interface>]"
 	echo "example :"
-	echo "  dummy.sh install"
-	echo "  dummy.sh uninstall"
+	echo "  dummy.sh start"
+	echo "  dummy.sh stop"
 	echo "  dummy.sh add dum0"
 	echo "  dummy.sh del dum0"
 }
@@ -15,13 +15,14 @@ if [ -z "$COMMAND" ]; then
   exit 1
 fi
 
-if [ "$COMMAND" == "install" ]; then
+if [ "$COMMAND" == "start" ]; then
   # echo "Installing dummy kernel module"
   sudo modprobe dummy
+  sudo ip link delete dummy0 type dummy
   exit 0
 fi
 
-if [ $COMMAND == "uninstall" ]; then
+if [ $COMMAND == "stop" ]; then
   # echo "Uninstalling dummy kernel module"
   sudo rmmod dummy
   exit 0
@@ -33,7 +34,7 @@ if [ "$COMMAND" == "add" ]; then
     echo "Interface not specified"
     exit 1
   fi
-  sudo ip link set name "$INTERFACE" dev dummy0
+  ip link add "$INTERFACE" type dummy
   sudo ifconfig "$INTERFACE" up
   exit 0
 fi
@@ -44,6 +45,7 @@ if [ "$COMMAND" == "del" ]; then
     echo "Interface not specified"
     exit 1
   fi
+  sudo ifconfig "$INTERFACE" down
   sudo ip link delete "$INTERFACE" type dummy
   exit 0
 fi
