e69c27a332933b06bcd70db873455f0d5577db61
gilgil <gilgil@gilgil.net>
2019-05-14 01:36:42 +0900

    Update pdu * files

--- src/net/pdu/gtcphdr.h
+++ src/net/pdu/gtcphdr.h
@@ -38,7 +38,7 @@ struct GTcpHdr final { // libnet_tcp_hdr // gilgil temp 2019.05.13
   uint16_t sum()   { return ntohs(sum_); }
   uint16_t urp()   { return ntohs(urp_); }
 
-  typedef enum {
+  enum Flag {
     Urg = 0x20,
     Ack = 0x10,
     Psh = 0x08,
