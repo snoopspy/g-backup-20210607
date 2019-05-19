b8683429c59da40d92019a01563ff178edc2da28
gilgil <gilgil@gilgil.net>
2019-05-19 17:36:23 +0900

    Change category name from Obj into obj

--- app/base/graph_exam/mygraph.cpp
+++ app/base/graph_exam/mygraph.cpp
@@ -7,7 +7,7 @@ MyFactory::MyFactory(QObject* parent) : GPluginFactory(parent) {
   qRegisterMetaType<GMyObjB*>();
   qRegisterMetaType<GMyObjC*>();
 
-  ItemCategory* category = new ItemCategory("Obj");
+  ItemCategory* category = new ItemCategory("obj");
   category->items_.push_back(new ItemNode(&GMyObjA::staticMetaObject));
   category->items_.push_back(new ItemNode(&GMyObjB::staticMetaObject));
   category->items_.push_back(new ItemNode(&GMyObjC::staticMetaObject));
