e5877b03ef930d2f0e615fdc8f8ae7f8c949d83f
gilgil <gilgil@gilgil.net>
2019-05-25 21:40:47 +0900

    Add all body codes in header file

--- src/net/packet/gpacketcast.h
+++ src/net/packet/gpacketcast.h
@@ -8,6 +8,8 @@
 //
 // ----------------------------------------------------------------------------
 
+#pragma once
+
 #include "net/packet/gethpacket.h"
 #include "net/packet/gippacket.h"
 #include "net/packet/gdot11packet.h"
@@ -16,9 +18,45 @@
 // ----------------------------------------------------------------------------
 // GPacketCast
 // ----------------------------------------------------------------------------
-namespace GPacketCast {
-  GEthPacket* toEth(GPacket* packet);
-  GIpPacket* toIp(GPacket* packet);
-  GDot11Packet* toDot11(GPacket* packet);
-  GNullPacket* toNull(GPacket* packet);
-}
+struct GPacketCast {
+  static GEthPacket* toEth(GPacket* packet) {
+    GEthPacket* res;
+    switch (packet->dataLinkType_) {
+      case GPacket::Eth: res = PEthPacket(packet); break;
+      case GPacket::Ip: res = PEthPacket(packet); break;
+      case GPacket::Dot11: return nullptr;
+      case GPacket::Null: return nullptr;
+    }
+    Q_ASSERT(dynamic_cast<GEthPacket*>(packet) != nullptr);
+    return res;
+  }
+
+  static GIpPacket* toIp(GPacket* packet) {
+    GIpPacket* res;
+    switch (packet->dataLinkType_) {
+      case GPacket::Eth: res = PIpPacket(packet); break;
+      case GPacket::Ip: res = PIpPacket(packet); break;
+      case GPacket::Dot11: return nullptr;
+      case GPacket::Null: return nullptr;
+    }
+    Q_ASSERT(dynamic_cast<GIpPacket*>(packet) != nullptr);
+    return res;
+  }
+
+  static GDot11Packet* toDot11(GPacket* packet) {
+    GDot11Packet* res;
+    switch (packet->dataLinkType_) {
+      case GPacket::Eth: return nullptr;
+      case GPacket::Ip: return nullptr;
+      case GPacket::Dot11: res = PDot11Packet(packet); break;
+      case GPacket::Null: return nullptr;
+    }
+    Q_ASSERT(dynamic_cast<GDot11Packet*>(packet) != nullptr);
+    return res;
+  }
+
+  static GNullPacket* toNull(GPacket* packet) {
+    Q_ASSERT(dynamic_cast<GNullPacket*>(packet) != nullptr);
+    return PNullPacket(packet);
+  }
+};
