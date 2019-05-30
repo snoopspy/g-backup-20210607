c2b95b964e87e37c65c3c04902d8c8de0d16700e
gilgil <gilgil@gilgil.net>
2019-05-30 22:28:33 +0900

    Add GWaitEvent, GStateWaitEvent and GCountWaitEvent class including unit test

--- src/base/sys/gwaitevent.h
+++ src/base/sys/gwaitevent.h
@@ -20,32 +20,126 @@
 // GWaitEvent
 // ----------------------------------------------------------------------------
 struct G_EXPORT GWaitEvent {
-  QWaitCondition wc_;
   QMutex m_;
+  QWaitCondition wc_;
 
-  bool wait(GDuration timeout = ULONG_MAX) {
+  virtual bool wait(GDuration timeout = ULONG_MAX) {
     m_.lock();
     bool res = wc_.wait(&m_, timeout);
     m_.unlock();
     return res;
   }
 
-  bool wait(QDeadlineTimer& dt) {
+  virtual bool wait(QDeadlineTimer& dt) {
     m_.lock();
     bool res = wc_.wait(&m_, dt);
     m_.unlock();
     return res;
   }
 
-  void wakeOne() {
+  virtual void wakeOne() {
     m_.lock();
     wc_.wakeOne();
     m_.unlock();
   }
 
-  void wakeAll() {
+  virtual void wakeAll() {
     m_.lock();
+    wc_.wakeAll();
+    m_.unlock();
+  }
+};
+
+// ----------------------------------------------------------------------------
+// GBoolWaitEvent
+// ----------------------------------------------------------------------------
+struct G_EXPORT GStateWaitEvent : GWaitEvent {
+  bool state_;
+
+  GStateWaitEvent(bool state = false) : state_(state) {}
+
+  bool wait(GDuration timeout = ULONG_MAX) override {
+    m_.lock();
+    if (state_) {
+      state_ = false;
+      m_.unlock();
+      return true;
+    }
+    bool res = wc_.wait(&m_, timeout);
+    m_.unlock();
+    return res;
+  }
+
+  bool wait(QDeadlineTimer& dt) override {
+    m_.lock();
+    if (state_) {
+      state_ = false;
+      m_.unlock();
+      return true;
+    }
+    bool res = wc_.wait(&m_, dt);
+    m_.unlock();
+    return res;
+  }
+
+  void wakeOne() override {
+    m_.lock();
+    state_ = true;
     wc_.wakeOne();
+    m_.unlock();
+  }
+
+  void wakeAll() override {
+    m_.lock();
+    state_ = true;
+    wc_.wakeAll();
+    m_.unlock();
+  }
+};
+
+// ----------------------------------------------------------------------------
+// GCountWaitEvent
+// ----------------------------------------------------------------------------
+struct G_EXPORT GCountWaitEvent : GWaitEvent {
+  int count_{0};
+
+  GCountWaitEvent(int count = 0) : count_(count) {}
+
+  bool wait(GDuration timeout = ULONG_MAX) override {
+    m_.lock();
+    if (count_ > 0) {
+      count_--;
+      m_.unlock();
+      return true;
+    }
+    bool res = wc_.wait(&m_, timeout);
+    m_.unlock();
+    return res;
+  }
+
+  bool wait(QDeadlineTimer& dt) override {
+    m_.lock();
+    if (count_ > 0) {
+      count_--;
+      m_.unlock();
+      return true;
+    }
+    bool res = wc_.wait(&m_, dt);
+    m_.unlock();
+    return res;
+  }
+
+  void wakeOne() override {
+    m_.lock();
+    count_++;
+    wc_.wakeOne();
+    m_.unlock();
+  }
+
+  void wakeAll() override {
+    m_.lock();
+    count_++;
+    wc_.wakeAll();
     m_.unlock();
   }
 };
