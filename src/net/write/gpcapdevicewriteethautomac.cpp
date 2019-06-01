f9fa1a037852034107d5c337e5d29975dfd54a7a
gilgil <gilgil@gilgil.net>
2019-06-01 18:59:25 +0900

    Add GPcapDeviceWriteEthAutoMac class

---
+++ src/net/write/gpcapdevicewriteethautomac.cpp
@@ -0,0 +1,118 @@
+#include "gpcapdevicewriteethautomac.h"
+#include <QMap>
+#include "net/gatm.h"
+
+// ----------------------------------------------------------------------------
+// GSyncPcapDeviceMgr
+// ----------------------------------------------------------------------------
+typedef QMap<QString /*devName*/, GSyncPcapDevice*> GSyncPcapDeviceMap;
+struct GSyncPcapDeviceMgr : GSyncPcapDeviceMap  {
+private: // singleton
+  GSyncPcapDeviceMgr() {}
+  virtual ~GSyncPcapDeviceMgr() {
+    GSyncPcapDeviceMap::iterator it = begin();
+    while (it != end()) {
+      delete *it;
+      it = erase(it);
+    }
+  }
+public:
+  static GSyncPcapDeviceMgr& instance() {
+    static GSyncPcapDeviceMgr syncPcapDeviceMgr;
+    return syncPcapDeviceMgr;
+  }
+};
+
+// ----------------------------------------------------------------------------
+// GPcapDeviceWriteEthAutoMac
+// ----------------------------------------------------------------------------
+bool GPcapDeviceWriteEthAutoMac::doOpen() {
+  if (!GPcapDeviceWrite::doOpen())
+    return false;
+
+  if (dataLinkType_ != GPacket::Eth) {
+    QString msg = QString("data link layer(%1) must be GPacket::Eth").arg(GPacket::dataLinkTypeToString(dataLinkType_));
+    SET_ERR(GErr::FAIL, msg);
+    return false;
+  }
+
+  Q_ASSERT(intf_ != nullptr);
+  myMac_ = intf_->mac();
+  if (myMac_.isClean()) {
+    QString msg = QString("not specified mac");
+    SET_ERR(GErr::VALUE_IS_NOT_ZERO, msg);
+    return false;
+  }
+
+  GSyncPcapDeviceMgr& mgr = GSyncPcapDeviceMgr::instance();
+  GSyncPcapDeviceMap::iterator it = mgr.find(devName_);
+  if (it == mgr.end()) {
+    GSyncPcapDevice* device = new GSyncPcapDevice;
+    device->devName_ = devName_;
+    if (!device->open()) {
+      err = device->err;
+      delete device;
+      return false;
+    }
+    it = mgr.insert(devName_, device);
+  }
+  device_ = it.value();
+  Q_ASSERT(device_ != nullptr);
+
+  return true;
+}
+
+bool GPcapDeviceWriteEthAutoMac::doClose() {
+  return GPcapDeviceWrite::doClose();
+}
+
+GPacket::Result GPcapDeviceWriteEthAutoMac::write(GPacket* packet) {
+  GBuf oldBuf = packet->buf_;
+  GBuf newBuf;
+  GEthHdr* ethHdr;
+  switch (packet->dataLinkType_) {
+    case GPacket::Eth:
+      newBuf = packet->buf_;
+      ethHdr = PEthHdr(oldBuf.data_);
+      break;
+    case GPacket::Ip:
+      newBuf.data_ = temp_;
+      newBuf.size_ = sizeof(GEthHdr) + oldBuf.size_;
+      Q_ASSERT(newBuf.size_ <= MAXBUF);
+      ethHdr = PEthHdr(temp_);
+      memcpy(temp_ + sizeof(GEthHdr), oldBuf.data_, oldBuf.size_);
+      break;
+    case GPacket::Dot11:
+    case GPacket::Null: {
+      QString msg = QString("not supported data link type(%1)").arg(GPacket::dataLinkTypeToString(dataLinkType_));
+      SET_ERR(GErr::NOT_SUPPORTED, msg);
+      return GPacket::Fail;
+    }
+  }
+  ethHdr->smac_ = myMac_;
+  GIpHdr* ipHdr = packet->ipHdr_;
+  if (ipHdr == nullptr) {
+    SET_ERR(GErr::VALUE_IS_NULL, "ip header is null");
+    return GPacket::Fail;
+  }
+  GIp dip = ipHdr->dip();
+  GIp adjIp = intf_->getAdjIp(dip);
+  GAtm& atm = GAtm::instance();
+  GAtmMap::iterator it = atm.find(adjIp);
+  if (it == atm.end()) {
+    GAtm atmOne;
+    GMac mac = atmOne.waitOne(adjIp, device_);
+    if (mac.cleanMac()) {
+      qCritical() << QString("can not resolve mac for ip %1").arg(QString(adjIp));
+      return GPacket::Fail;
+    }
+    it = atm.insert(adjIp, mac);
+  }
+  GMac dmac = it.value();
+  ethHdr->dmac_ = dmac;
+  ethHdr->type_ = htons(type_);
+  packet->buf_ = newBuf;
+  GPacket::Result res = GPcapDeviceWrite::write(packet);
+  packet->buf_ = newBuf;
+  return res;
+}
---
+++ src/net/write/gpcapdevicewriteethautomac.h
@@ -0,0 +1,42 @@
+// ----------------------------------------------------------------------------
+//
+// G Library
+//
+// http://www.gilgil.net
+//
+// Copyright (c) Gilbert Lee All rights reserved
+//
+// ----------------------------------------------------------------------------
+
+#include "gpcapdevicewrite.h"
+#include "net/capture/gsyncpcapdevice.h"
+
+// ----------------------------------------------------------------------------
+// GPcapDeviceWriteEthAutoMac
+// ----------------------------------------------------------------------------
+struct G_EXPORT GPcapDeviceWriteEthAutoMac : GPcapDeviceWrite {
+  Q_OBJECT
+  Q_PROPERTY(quint16 type MEMBER type_)
+
+public:
+  uint16_t type_{GEthHdr::Ip4};
+
+public:
+  Q_INVOKABLE GPcapDeviceWriteEthAutoMac(QObject* parent = nullptr) : GPcapDeviceWrite(parent) {
+    dataLinkType_ = GPacket::Eth;
+  }
+  ~GPcapDeviceWriteEthAutoMac() override { close(); }
+
+protected:
+  bool doOpen() override;
+  bool doClose() override;
+
+protected:
+  GMac myMac_;
+  GSyncPcapDevice* device_{nullptr};
+  static const int MAXBUF = 65536;
+  gbyte temp_[MAXBUF];
+
+public slots:
+  GPacket::Result write(GPacket* packet);
+};
