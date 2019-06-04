4293d4cd7d6f9e9df884fe31896945a3006bb3dd
gilgil <gilgil@gilgil.net>
2019-06-04 18:09:23 +0900

    Add make_vs.bat

---
+++ make-vs.bat
@@ -0,0 +1,34 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
+set path=%path%;C:\Qt\5.12.2\msvc2017_64\bin
+
+rem ***************************************************************************
+rem lib
+rem ***************************************************************************
+cd lib
+  mkdir libg
+  cd libg
+  qmake ../libg.pro QMAKE_CXXFLAGS+=/MP
+  nmake
+  cd ..
+  mkdir libg_gui
+  cd libg_gui
+  qmake ../libg_gui.pro QMAKE_CXXFLAGS+=/MP
+  nmake
+  cd ..
+cd ..
+
+rem ***************************************************************************
+rem plugin
+rem ***************************************************************************
+cd plugin
+qmake g_plugin.pro QMAKE_CXXFLAGS+=/MP
+nmake
+cd ..
+
+rem ***************************************************************************
+rem app
+rem ***************************************************************************
+cd app
+qmake g_app.pro QMAKE_CXXFLAGS+=/MP
+nmake
+cd ..
