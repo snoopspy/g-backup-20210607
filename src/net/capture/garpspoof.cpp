8707933c67f81328bbb59cab454fb385d405f713
gilgil <gilgil@gilgil.net>
2019-08-16 06:39:14 +0900

    Change GArpSpoof

--- src/net/capture/garpspoof.cpp
+++ src/net/capture/garpspoof.cpp
@@ -3,18 +3,92 @@
 // ----------------------------------------------------------------------------
 // GArpSpoof
 // ----------------------------------------------------------------------------
-GArpSpoof::GArpSpoof(QObject* parent) : GPcapDevice(parent) {
-
+GArpSpoof::GArpSpoof(QObject* parent) : GSyncPcapDevice(parent) {
+	qDebug() << ""; // gilgil temp 2019.08.16
 }
 
 GArpSpoof::~GArpSpoof() {
+	qDebug() << ""; // gilgil temp 2019.08.16
   close();
 }
 
 bool GArpSpoof::doOpen() {
-  return GPcapDevice::doOpen();
+	if (!GPcapDevice::doOpen()) return false;
+
+	GNetIntf* intf = GNetIntfs::instance().findByName(devName_);
+	if (intf == nullptr) {
+		SET_ERR(GErr::DEVICE_NOT_SPECIFIED, QString("cat not find device for ").arg(devName_));
+		return false;
+	}
+	intf_ = *intf;
+
+	sessionList_.clear();
+	sessionMap_.clear();
+
+	for (GAtm::iterator it = atm_.begin(); it != atm_.end();) {
+		GMac mac = it.value();
+		if (mac == GMac::cleanMac())
+			it = atm_.erase(it);
+		else
+			it++;
+	}
+	for (GObj* obj: propSessions_) {
+		GArpSpoofSession* propSession = PArpSpoofSession(obj);
+
+		GIp senderIp = propSession->senderIp_;
+		GMac senderMac = propSession->senderMac_;
+		GIp targetIp = propSession->targetIp_;
+		GMac targetMac = propSession->targetMac_;
+
+		if (senderIp == intf_.ip()) {
+			QString msg = QString("sender(%1) can not be my ip").arg(QString(senderIp));
+			SET_ERR(GErr::FAIL, msg);
+			return false;
+		}
+
+		if (targetIp == intf_.ip()) {
+			QString msg = QString("target(%1) can not be my ip").arg(QString(senderIp));
+			SET_ERR(GErr::FAIL, msg);
+			return false;
+		}
+
+		if (senderIp == targetIp) {
+			QString msg = QString("sender(%1) and target(%2) can not be same").arg(QString(senderIp), QString(targetIp));
+			SET_ERR(GErr::FAIL, msg);
+			return false;
+		}
+
+		Session session;
+		session.senderIp_ = senderIp;
+		session.senderMac_ = senderMac;
+		session.targetIp_ = targetIp;
+		session.targetMac_ = targetMac;
+
+		sessionList_.push_back(session);
+		sessionMap_[GFlow::IpFlowKey(session.senderIp_, session.targetIp_)] = session;
+
+		if (atm_.find(senderIp) == atm_.end())
+			atm_[session.senderIp_] = session.senderMac_;
+		if (atm_.find(targetIp) == atm_.end())
+			atm_[session.targetIp_] = session.targetMac_;
+	}
+
+	if (!atm_.waitAll(this)) {
+		QString msg = "can not find all host ";
+		for (GAtm::iterator it = atm_.begin(); it != atm_.end(); it++) {
+			GMac mac = it.value();
+			if (mac.isClean()) {
+				GIp ip = it.key();
+				msg += QString(ip) += " ";
+			}
+		}
+		SET_ERR(GErr::FAIL, msg);
+		return false;
+	}
+	return true;
 }
 
 bool GArpSpoof::doClose() {
+	qDebug() << ""; // gilgil temp 2019.08.16
   return GPcapDevice::doClose();
 }
--- src/net/capture/garpspoof.h
+++ src/net/capture/garpspoof.h
@@ -10,13 +10,45 @@
 
 #pragma once
 
-#include "gpcapdevice.h"
+#include "gsyncpcapdevice.h"
+#include "net/gatm.h"
+#include "net/flow/gflowkey.h"
+
+// ----------------------------------------------------------------------------
+// GArpSpoofSession
+// ----------------------------------------------------------------------------
+struct GArpSpoofSession : GObj {
+	Q_OBJECT
+	Q_PROPERTY(QString senderIp READ getSenderIp WRITE setSenderIp)
+	Q_PROPERTY(QString senderMac READ getSenderMac WRITE setSenderMac)
+	Q_PROPERTY(QString targetIp READ getTargetIp WRITE setTargetIp)
+	Q_PROPERTY(QString targetMac READ getTargetMac WRITE setTargetMac)
+
+	QString getSenderIp() { return QString(senderIp_); }
+	void setSenderIp(QString value) { senderIp_ = value; }
+	QString getSenderMac() { return QString(senderMac_); }
+	void setSenderMac(QString value) { senderMac_ = value; }
+	QString getTargetIp() { return QString(targetIp_); }
+	void setTargetIp(QString value) { targetIp_ = value; }
+	QString getTargetMac() { return QString(targetMac_); }
+	void setTargetMac(QString value) { targetMac_ = value; }
+
+public:
+	GIp senderIp_{uint32_t(0)};
+	GMac senderMac_{GMac::cleanMac()};
+	GIp targetIp_{uint32_t(0)};
+	GMac targetMac_{GMac::cleanMac()};
+};
+typedef GArpSpoofSession *PArpSpoofSession;
 
 // ----------------------------------------------------------------------------
 // GArpSpoof
 // ----------------------------------------------------------------------------
-struct G_EXPORT GArpSpoof : GPcapDevice {
+struct G_EXPORT GArpSpoof : GSyncPcapDevice {
   Q_OBJECT
+	Q_PROPERTY(GObjRefArrayPtr sessions READ getSessions)
+
+	GObjRefArrayPtr getSessions() { return &propSessions_; }
 
 public:
   Q_INVOKABLE GArpSpoof(QObject* parent = nullptr);
@@ -25,4 +57,20 @@ public:
 protected:
   bool doOpen() override;
   bool doClose() override;
+
+public:
+	GObjRefArray<GArpSpoofSession> propSessions_; // for property
+
+protected:
+	GNetIntf intf_;
+
+	struct Session {
+		GIp senderIp_;
+		GMac senderMac_;
+		GIp targetIp_;
+		GMac targetMac_;
+	};
+	QList<Session> sessionList_; // for arp infect and recover
+	QMap<GFlow::IpFlowKey, Session> sessionMap_; // for relay
+	GAtm atm_;
 };
